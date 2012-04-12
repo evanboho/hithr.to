e8c27303f0880dbfacce637c52c921dce46cf4c0
Evan Kuchar <evan.kuchar@gmail.com>
2012-04-12 16:10:45 -0500

    session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Hithr::Application.config.session_store :cookie_store, key: '_hithr_session', :domain => :all
+Hithr::Application.config.session_store :cookie_store, key: '_hithr_session', :domain => '.hithr.to'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
