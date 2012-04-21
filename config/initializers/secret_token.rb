435a629eeca82e64d8efb8d5d59a4b5c54918909
Evan Kuchar <evan.kuchar@gmail.com>
2012-04-21 17:07:33 -0500

    secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,5 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-# Hithr::Application.config.secret_token = ENV['SECRET_TOKEN']
-Hithr::Application.config.secret_token = "fEkjfoOdkfjsdfwewl34khpolkjJsldkjfmzlkjsoWslkdjjklSdlfkjaSsP"
+Hithr::Application.config.secret_token = ENV['SECRET_TOKEN']
+# Hithr::Application.config.secret_token = "fEkjfoOdkfjsdfwewl34khpolkjJsldkjfmzlkjsoWslkdjjklSdlfkjaSsP"
