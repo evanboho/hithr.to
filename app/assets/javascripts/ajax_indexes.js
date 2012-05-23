ad6e1adfac675bdad71db46c7825c88c2c91d27a
Evan Kuchar <evan.kuchar@gmail.com>
2012-05-22 20:04:10 -0500

    popup

--- app/assets/javascripts/ajax_indexes.js
+++ app/assets/javascripts/ajax_indexes.js
@@ -35,4 +35,16 @@ $(function() {
 	  return false;
 	});
 	
+	$('a.popup').click( function(e) {
+	  popupCenter($(this).attr('href'), $(this).attr("data-width"), $(this).attr('data-height'), 'authPopup');
+	  e.stopPropogation(); 
+	  return false;
+	});
+	
 });
+
+function popupCenter(url, width, height, name) {
+  var left = (screen.width/2)-(width/2);
+  var top = (screen.height/2)-(height/2);
+  return window.open(url, name, "menubar=no,toolbar=no,status=no,width="+width+",height="+height+",toolbar=no,left="+left+",top="+top);
+}
